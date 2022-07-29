56c366ab8e839012652d1c678390645f667c0c28
raihan512 <raihangazi1024@Gmail.com>
2022-07-30 05:06:49 +0600

    Third solve added

---
+++ 03.triangleArea.js
@@ -0,0 +1,10 @@
+// Write a function that takes the base and height of a triangle and return its area.
+function triangleArea(base, height) {
+    return .5 * base * height;
+}
+
+let triangleOne = triangleArea(7, 4);
+let triangleTwo = triangleArea(10, 10);
+
+console.log(triangleOne);
+console.log(triangleTwo);
